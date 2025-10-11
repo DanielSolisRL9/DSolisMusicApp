7d59d7a48501c63c46a1a2032e7f032153660348
DanielRl9 <dsp4346895@outlook.com>
2025-10-11 14:32:41 -0600

    Creacion del header

---
+++ app/src/main/java/com/example/dsolismusicapp/Components/LazyRowAlbum.kt
@@ -0,0 +1,2 @@
+package com.example.dsolismusicapp.Components
+
--- app/src/main/java/com/example/dsolismusicapp/Screens/HomeScreen.kt
+++ app/src/main/java/com/example/dsolismusicapp/Screens/HomeScreen.kt
@@ -1,9 +1,15 @@
 package com.example.dsolismusicapp.Screens
 
+import android.graphics.Color
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +20,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.dsolismusicapp.Components.Header
 import com.example.dsolismusicapp.Models.Albums
 import com.example.dsolismusicapp.Services.AlbumService
 import com.example.dsolismusicapp.ui.theme.DSolisMusicAppTheme
@@ -54,26 +64,49 @@ fun HomeScreen(){
         }
 
     }
-    if (loading){
-        Box (
+//    if (loading){
+//        Box (
+//            modifier = Modifier
+//                .fillMaxSize(),
+//            contentAlignment = Alignment.Center
+//        ){
+//            CircularProgressIndicator()
+//        }
+//    }else {
+//
+//    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(androidx.compose.ui.graphics.Color.White)
+            .padding(horizontal = 20.dp)
+    ) {
+        Header(
             modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
+                .weight(1f)
+        )
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp)
+                .weight(3f)
         ){
-            CircularProgressIndicator()
+            Text("Albums",
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp
+                )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text("See more",
+                fontSize = 18.sp,
+                color = androidx.compose.ui.graphics.Color.Magenta
+                )
         }
-    }else {
-        if (albums.isNotEmpty()) {
-            val album = albums[0] // tomamos el primero
-            Column {
-                Text(text = "Artista: ${album.artist}")
-                Text(text = "Título: ${album.title}")
-                Text(text = "Descripción: ${album.description}")
-            }
-        } else {
-            Text("No se encontraron álbumes")
-        }
+
+
     }
+
 
 }
 
